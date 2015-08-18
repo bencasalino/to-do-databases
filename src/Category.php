26a4596190922d6dace96c3f551042b3e4b8c591
Ike Mattice & Ben Casalino <student@epicodus.com>
2015-08-18 15:35:38 -0700

    link databases.

---
+++ src/Category.php
@@ -0,0 +1,87 @@
+<?php
+    class Category
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO categories (name) VALUES ('{$this->getName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        function getTasks()
+    {
+        $tasks = array();
+        //goes to database and searches for the input id
+        $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks WHERE category_id = {$this->getId()};");
+        //loops through search results and pulls out correct property values
+        foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $id = $task['id'];
+            $category_id = $task['category_id'];
+            //creates a new Task from the found property values
+            $new_task = new Task($description, $id, $category_id);
+            //this returns multiple matches and returns all matches
+            array_push($tasks, $new_task);
+        }
+        return $tasks;
+    }
+
+        static function getAll()
+        {
+            $returned_categories = $GLOBALS['DB']->query("SELECT * FROM categories;");
+            $categories = array();
+
+            foreach ($returned_categories as $category) {
+                $name = $category['name'];
+                $id = $category['id'];
+                $new_category = new Category($name, $id);
+                array_push ($categories, $new_category);
+            }
+
+            return $categories;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM categories;");
+        }
+
+        static function find($search_id)
+        {
+            $found_category = null;
+            $categories = Category::getAll();
+            foreach($categories as $category) {
+                $category_id = $category->getId();
+                if ($category_id == $search_id) {
+                    $found_category = $category;
+                }
+            }
+
+            return $found_category;
+        }
+    }
+
+?>
---
+++ tests/CategoryTest.php
@@ -0,0 +1,124 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Category.php";
+    require_once "src/Task.php";
+
+    $server = 'mysql:host=localhost; dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CategoryTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Task::deleteAll();
+            Category::deleteAll();
+        }
+
+        function test_getName()
+        {
+            $name = "Work stuff";
+            $test_Category = new Category($name);
+
+            $result = $test_Category->getName();
+
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId()
+        {
+            $name = "Work stuff";
+            $id = 1;
+            $test_Category = new Category($name, $id);
+
+            $result = $test_Category->getId();
+
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function testGetTasks()
+        {
+            //Arrange
+            $name = "Work stuff";
+            $id = null;
+            $test_category = new Category ($name, $id);
+            $test_category->save();
+
+            $test_category_id = $test_category->getId();
+
+            $description = "Email client";
+            $test_task = new Task($description, $id, $test_category_id);
+            $test_task->save();
+
+            $description2 = "Meet with boss";
+            $test_task2 = new Task($description2, $id, $test_category_id);
+            $test_task2->save();
+
+            //Act
+            $result = $test_category->getTasks();
+
+            //Assert
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+
+        function test_save()
+        {
+            $name = "Work stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+
+            $result = Category::getAll();
+
+            $this->assertEquals($test_Category, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            $name = "Work stuff";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            $result = Category::getAll();
+
+            $this->assertEquals([$test_Category, $test_Category2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            $name = "Wash the dog";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            Category::deleteAll();
+            $result = Category::getAll();
+
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            $name = "Wash the dog";
+            $name2 = "Home stuff";
+            $test_Category = new Category($name);
+            $test_Category->save();
+            $test_Category2 = new Category($name2);
+            $test_Category2->save();
+
+            $result = Category::find($test_Category->getId());
+
+            $this->assertEquals($test_Category, $result);
+        }
+
+    }
+?>
